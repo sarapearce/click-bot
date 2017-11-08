b2154412c3c85408d8b58a73935681ca8bce089d
Sara Pearce <sarapearce3.14@gmail.com>
2017-11-08 10:03:01 -0600

    deleted test note and code

--- small-click-loop.js
+++ small-click-loop.js
@@ -20,9 +20,5 @@ for (var i = 0; i < 15; i++) {
             console.log('test3');
         }
     }, 1700*i);
-    
-    //this is for testing that the clicker has completed. for some reason, its inconsistently completing
-    if (i == "14") {
-        console.log("end");
-    }
+   
 }
